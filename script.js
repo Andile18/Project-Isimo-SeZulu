17e8233024015da397311daf24fed3353bb2a5e6
Andile18 <zama.andile18@gmail.com>
2024-03-30 23:06:01 +0200

    Added search functionallity using the API

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
+function updateWeatherPercent(response){
+
+    let temperatureElement =  document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#app-city");
+   
+   cityElement.innerHTML = response.data.city;
+   temperatureElement.innerHTML = Math.round(temperature);
+   
+  
+}
+
+
+
+
+function searchCity (city){
+let apiKey = "2tb5293558cf27710004o841aef8f671";
+let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+axios.get(apiUrl).then(updateWeatherPercent); 
+}
+
+
+
 function submitSearchHandle(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityEelement = document.querySelector("#app-city");
-    cityEelement.innerHTML = searchInput.value
+   
+    
+    searchCity(searchInput.value);
 }
+   
+
 
 
 let searchFormElement= document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitSearchHandle);
+
+searchCity ("Cape Town");
