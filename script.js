e541dfd19291610b39fe57af394646dff9d6c54c
Andile18 <zama.andile18@gmail.com>
2024-03-31 02:08:23 +0200

    injected more weather data

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ function updateWeatherPercent(response){
     let temperatureElement =  document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#app-city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let speedElement = document.querySelector("#speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+
+
    
    cityElement.innerHTML = response.data.city;
+   descriptionElement.innerHTML = response.data.condition.description;
+   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+   timeElement.innerHTML = formattedDate(date);
+   speedElement.innerHTML= `${response.data.wind.speed}km/h`;
    temperatureElement.innerHTML = Math.round(temperature);
    
   
@@ -36,3 +47,22 @@ let searchFormElement= document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitSearchHandle);
 
 searchCity ("Cape Town");
+
+function formattedDate (date){
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+
+    if (minutes < 10) {
+        minutes =` 0${minutes}`;
+    
+
+if (hours <10 ){
+    hours = ` 0${hours}`
+}
+    }
+
+
+     return  `${day} ${hours} : ${minutes}`
+}
