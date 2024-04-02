bc6ccd5aabaf6306580f4a0da6204558af465eef
Andile18 <zama.andile18@gmail.com>
2024-04-02 15:58:21 +0200

    Injected the forecast HTML from Javascript

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function updateWeatherPercent(response){
     let speedElement = document.querySelector("#speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
-    let stickerElement = document.querySelector ("#sticker")
+    let iconElement = document.querySelector ("#icon")
 
 
    
@@ -19,11 +19,46 @@ function updateWeatherPercent(response){
    speedElement.innerHTML= `${response.data.wind.speed}km/h`;
    temperatureElement.innerHTML = Math.round(temperature);
    
-   stickerElement.innerHTML = ` <img src ="${response.data.condition.icon_url}" class= "sticker"/>`
-
-  
+   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+   getForecast(response.data.city);
 }
 
+
+
+function getForecast(city) {
+    let apiKey = "2tb5293558cf27710004o841aef8f671";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+  }
+
+
+function displayForecast(response) {
+    console.log(response.data);
+  
+    let days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+    let forecastHtml = "";
+  
+    days.forEach(function (day) {
+      forecastHtml =
+        forecastHtml +
+        `
+        <div class="weather-forecast-day">
+          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-icon">🌤️</div>
+          <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+              <strong>15º</strong>
+            </div>
+            <div class="weather-forecast-temperature">9º</div>
+          </div>
+        </div>
+      `;
+    });
+  
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+  }
+  
 
 
 
