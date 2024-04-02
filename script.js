5f12716b5dbe876a4c24728b2be973bf3c8ea610
Andile18 <zama.andile18@gmail.com>
2024-04-02 19:47:26 +0200

    Updated Script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function updateWeatherPercent(response){
 
     let temperatureElement =  document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
-    let cityElement = document.querySelector("#app-city");
+    let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let speedElement = document.querySelector("#speed");
@@ -13,7 +13,7 @@ function updateWeatherPercent(response){
 
    
    cityElement.innerHTML = response.data.city;
-   descriptionElement.innerHTML = response.data.condition.description;
+   descriptionElement= response.data.condition.description;
    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
    timeElement.innerHTML = formattedDate(date);
    speedElement.innerHTML= `${response.data.wind.speed}km/h`;
@@ -64,7 +64,7 @@ function displayForecast(response) {
 
 function searchCity (city){
 let apiKey = "2tb5293558cf27710004o841aef8f671";
-let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(updateWeatherPercent); 
 }
 
@@ -85,6 +85,7 @@ let searchFormElement= document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitSearchHandle);
 
 searchCity ("Cape Town");
+
 
 function formattedDate (date){
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
